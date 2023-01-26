f48fba8b5deeaf80cc4cca3cefd5af474027be2c
Nathanael Metke <nathanael.metke@uconn.edu>
2023-01-25 20:53:51 -0500

    Starting the book uploading process

--- components/search.js
+++ components/search.js
@@ -1,5 +1,6 @@
 import styles from '../styles/Search.module.css';
 import { bookSearch } from '../lib/BookGetter';
+import { addBook } from '../lib/BookSetter';
 import { useState } from 'react';
 import {Image} from 'next/image';
 import {Link} from 'next/link';
@@ -26,19 +27,27 @@ export default function Search(){
 
     }
     
+    async function  bookNavigate(e, book){
+        e.preventDefault();
+        //console.log(book);
+        await addBook(book);
+
+    }
+
     function handleKeyPress (event)  {
         if(event.key === 'Enter'){
           setShowMenu(true)
           
             
-
+    
           bookSearch(query).then(res => {
             console.log(res);
             setBookList(res.items.map((item)=>{
                 return {
                     "title": item.volumeInfo.title,
                     "authors": item.volumeInfo.authors,
-                    "cover": item.volumeInfo.imageLinks
+                    "cover": item.volumeInfo.imageLinks,
+                    "volumeInfo": item.volumeInfo
                 }
             }))
           })
@@ -50,12 +59,23 @@ export default function Search(){
         <div className={styles.vert_container}>
             <div className={styles.container}>
                 <input onKeyDown={handleKeyPress} value={query} onChange={handleSearchChange} className={styles.searchbar} placeholder='Search Books'></input>
-                <button onClick={()=>bookSearch(query)} className={styles.searchbutton}>Search</button>
+                <button onClick={(e)=>{e.preventDefault(); bookSearch(e,query)}} className={styles.searchbutton}>Search</button>
                 
             </div>
-            {showMenu && (bookListView.length > 0) ? (
-                <a href="/" className={styles.bookMenu}>{bookListView}</a>
-            ) : null}
+            {showMenu && (bookListView.length > 0) ? 
+            bookList.map((book)=> 
+                
+                <a onClick={(e)=>bookNavigate(e, book.volumeInfo)} href="/" className={styles.bookMenu}>
+                    <div className="bookListEntry">
+                        <div className="bookInfo">
+                            <h3>{book.title}</h3> 
+                            <h5>{book.authors[0]}</h5>
+                        </div>
+                        <hr></hr>
+                    </div>
+                    </a>)
+                
+             : null}
         </div>
     )
 }
